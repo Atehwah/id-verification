304b583ed101cf6a331b48513dba9160b18a58ca
Fonchu <fonchu@Fonchus-MacBook-Pro.local>
2025-11-12 23:19:16 +0100

    updates

--- src/selfie-capture.tsx
+++ src/selfie-capture.tsx
@@ -217,8 +217,6 @@ const DocumentSelfieVerifier = () => {
   const s3 = new S3Client({
         region: 'us-east-1',
         credentials: {
-          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
         }
       });
   const uploadFile = async(folderName: string, file: File) => {
